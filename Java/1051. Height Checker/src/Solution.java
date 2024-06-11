9661c8f2c311f7fddfee5ec622f3157935728154
Saed Sousa <saed.sousa@hotmail.com>
2024-06-10 21:53:40 -0300

    1051

--- Java/1051. Height Checker/src/Solution.java
+++ Java/1051. Height Checker/src/Solution.java
@@ -1,3 +1,15 @@
-public class Solution {
+import java.util.Arrays;
 
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] ordened = heights.clone();
+        Arrays.sort(ordened);
+        int numberOfErrors = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if(ordened[i] != heights[i]) {
+                numberOfErrors++;
+            }
+        }
+        return numberOfErrors;
+    }
 }
