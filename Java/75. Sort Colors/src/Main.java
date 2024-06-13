3cd3db1f5eae9df39d87c86861df195c576d9582
Saed Sousa <saed.sousa@hotmail.com>
2024-06-13 20:48:58 -0300

    75 java

---
+++ Java/75. Sort Colors/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {2,0,2,1,1,0};
+        new Solution().sortColors(a);
+    }
+}
---
+++ Java/75. Sort Colors/src/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < nums.length-1; j++) {
+                if(nums[j] > nums[i]) {
+                    var temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+    }
+}
